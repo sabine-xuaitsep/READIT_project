a7be19a0a74bfaca383441ae5edf51c538f85ecb
sabine-xuaitsep <sabinepestiaux@gmail.com>
2021-05-20 09:53:25 +0200

    Template integration and fragmentation

--- app/views/templates/index.php
+++ app/views/templates/index.php
@@ -2,3 +2,30 @@
 /* 
   ./app/views/templates/index.php
 */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    
+    <?php include_once '../app/views/templates/partials/_head.php'; ?>
+
+  </head>
+  <body>
+
+    <?php include '../app/views/templates/partials/_nav.php'; ?>
+
+    <?php include '../app/views/templates/partials/_hero.php'; ?>
+
+    <?php include '../app/views/templates/partials/_main.php'; ?>
+
+    <?php include '../app/views/templates/partials/_footer.php'; ?>
+
+
+  <!-- loader -->
+  <?php include '../app/views/templates/partials/_loader.php'; ?>
+
+  <?php include '../app/views/templates/partials/_scripts.php'; ?>
+
+  </body>
+</html>
---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+  ./app/views/templates/partials/_main.php
+*/
+?>
+
+<section class="ftco-section ftco-degree-bg">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 ftco-animate">
+        <?php echo $content; ?>
+      </div>
+
+      <!-- .col-md-8 -->
+      <?php include '../app/views/templates/partials/_aside.php'; ?>
+
+    </div>
+  </div>
+</section> <!-- .section -->
